10f8e24aed0105882eaec4ac09cfce1605f96970
cbnuLeehyunwoo <henow123@naver.com>
2025-04-20 02:51:19 +0900

    쿵야사진 추가

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { getSortedPostsData } from '../lib/posts'
 
 export default async function Home() {
@@ -6,6 +7,12 @@ export default async function Home() {
   return (
     <main>
       <h1>블로그</h1>
+      <Image 
+          src="public\images\29QRYNO2QS_1.png" // public/images 폴더에 넣은 이미지 경로
+          alt="쿵쿵야"
+          width={1200} // 이미지의 너비 (픽셀 단위)
+          height={600} // 이미지의 높이 (픽셀 단위)
+        />
       <ul>
         {allPostsData.map(({ id, title, date }) => (
           <li key={id}>
